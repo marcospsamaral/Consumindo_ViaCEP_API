18431c8b55eb2764139775fab057739b3664c41c
Marcos <marcos.pkinho@gmail.com>
2025-05-20 17:37:36 -0300

    Utilizando api viaCep

---
+++ src/br/com/alura/buscacep/modulos/EnderecoViaCep.java
@@ -0,0 +1,4 @@
+package br.com.alura.buscacep.modulos;
+
+public record EnderecoViaCep(String cep){//, String logradouro, String bairro, String localidade, String estado, String regiao) {
+}
---
+++ src/br/com/alura/buscacep/principal/Principal.java
@@ -0,0 +1,44 @@
+package br.com.alura.buscacep.principal;
+
+import br.com.alura.buscacep.modulos.Endereco;
+import br.com.alura.buscacep.modulos.EnderecoViaCep;
+import com.google.gson.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String leitura = scanner.nextLine();
+        List<Endereco> enderecos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://viacep.com.br/ws/"+leitura+"/json/")).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(gson.fromJson(response.body(), EnderecoViaCep.class));
+            //String jsonCorrigido = gson.fromJson(response.body().toString(), String.class);
+            EnderecoViaCep enderecoViaCep = gson.fromJson(response.body(), EnderecoViaCep.class);
+            System.out.println(enderecoViaCep.cep());
+            Endereco endereco = new Endereco(enderecoViaCep);
+            enderecos.add(endereco);
+
+        } catch (Exception e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+        System.out.println(enderecos);
+        FileWriter enderecoJSON = new FileWriter("endereco.json");
+        enderecoJSON.write(gson.toJson(enderecos));
+        enderecoJSON.close();
+    }
+}
